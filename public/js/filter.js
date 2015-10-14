891f0d10edd7a9f16c1ab5f2422fbe2293eeff20
lucarin91 <lucarin91@gmail.com>
2015-10-14 15:34:03 +0200

    new filter and service

---
+++ public/js/filter.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* Filters */
+
+angular.module('mySiteFilter', [])
+
+.filter('interpolate', function (version) {
+    return function (text) {
+      return String(text).replace(/\%VERSION\%/mg, version);
+    };
+})
+
+.filter('formatDate', function() {
+  function pad(s) { return (s < 10) ? '0' + s : s; }
+  return function (date) {
+      var start = new Date(date.begin);
+      var end = new Date(date.end);
+      return [[pad(start.getDate()), pad(start.getMonth()+1), start.getFullYear()].join('/'),
+              [pad(end.getDate()), pad(end.getMonth()+1), end.getFullYear()].join('/')].join(' - ');
+  }
+})
+
+.filter('langFilter', ['languageServ', function(languageServ){
+  return function(item){
+    return item[languageServ.get()];
+  };
+}]);
---
+++ public/js/service.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/* Services */
+angular.module('mysiteService', ['ngResource'])
+//for the language of the site
+.factory('languageServ', [function(){
+  var language = 'ita';
+  return {
+    setIta:function(){
+      language='ita';
+    },
+    setEng: function(){
+      language='eng';
+    },
+    get: function(property){
+      return language;
+    }
+  }
+}])
+
+.factory('APIService',['$resource',
+  function($resource){
+    return {
+      me: $resource('/api/me'),
+      edu: $resource('/api/education'),
+      exp: $resource('/api/experience'),
+      skill: $resource('/api/skills'),
+      skillCat: $resource('/api/skillscat'),
+      pro: $resource('/api/projects'),
+      proCat: $resource('/api/projectscat')
+    }
+}])
+/*
+.factory('EducationService',['$resource',
+  function($resource){
+    return $resource('/api/education', {
+      get: {method:'GET', isArray:true}
+    });
+}])
+
+.factory('ExperienceService',['$resource',
+  function($resource){
+    return $resource('/api/:language/experience',{language:'@lang'}, {
+      getExperience: {method:'GET', isArray:true}
+    });
+}])
+
+
+
+.factory('SkillsService',['$resource',
+  function($resource){
+    return $resource('/api/:language/skills',{language:'@lang'}, {
+      getSkills: {method:'GET', isArray:true}
+    });
+}])
+
+.factory('ProjectsService',['$resource',
+  function($resource){
+    return $resource('/api/:language/projects',{language:'@lang'}, {
+      getProjects: {method:'GET', isArray:true}
+    });
+}])
+*/
+// Demonstrate how to register services
+// In this case it is a simple value service.
+.factory('JsonLoader',['$http',
+  function($http){
+    return {
+      get: function(path){
+        return $http({
+          method: 'GET',
+          url: path
+        });
+      }
+    }
+}]);
