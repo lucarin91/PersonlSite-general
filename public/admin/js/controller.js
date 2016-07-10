106808e29887c5a215cce3dbd3d5bdbb4455a87d
lucarin91 <lucarin91@gmail.com>
2016-07-10 11:16:14 +0200

    update login page

--- public/admin/js/controller.js
+++ public/admin/js/controller.js
@@ -1,16 +1,23 @@
 angular.module('PSAcontroller', [])
 
-.controller('LoginCtrl', function($scope, $state, $http, TokenService){
-  $scope.error = '';
-  $scope.login = function(user, pass){
-    console.log("username: "+ user + " - password: "+ pass);
-    $http.post('/api/authenticate', {user:user,password:pass}).success(function(data){
-      console.log(data);
-      if (data.ok==1){
-        TokenService.set(data.token);
-        console.log($state);
-        $state.go('dashboard');
-      } else $scope.error = "wrong username or password";
-    });
-  };
+.controller('LoginCtrl', function($scope, $state, $http, TokenService) {
+    $scope.error = '';
+    $scope.login = function(user, pass) {
+        console.log("username: " + user + " - password: " + pass);
+        $http.post('/api/authenticate', {
+            user: user,
+            password: pass
+        }).success(function(data) {
+            console.log(data);
+            if (data.ok == 1) {
+                TokenService.set(data.token);
+                console.log($state);
+                $state.go('dashboard');
+            } else {
+                $scope.error = "wrong username or password ";
+                $scope.user = '';
+                $scope.pass = '';
+            }
+        });
+    };
 });
